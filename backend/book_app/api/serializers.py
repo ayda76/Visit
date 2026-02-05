8326b86e2fff0e98a7b2d28b8ac3eb929daa9445
ayda76 <aydavahedfrough@yahoo.com>
2026-02-05 06:29:26 +0000

    added validation and transaction to appointment serializer

--- backend/book_app/api/serializers.py
+++ backend/book_app/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from book_app.models import *
-
+from django.db import transaction, IntegrityError
 
   
 class AppointmentSerializer(serializers.ModelSerializer):
@@ -9,3 +9,22 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
+        validators=[]
+        
+    def create(self,validated_data):
+        provider_related=validated_data['provider_related']
+        date=validated_data['date']
+        start_time=validated_data['start_time']
+        end_time=validated_data['end_time']
+            
+            
+        with transaction.atomic():
+            reserved_appointment=Appointment.objects.filter(provider_related=provider_related,date=date,start_time=start_time,end_time=end_time, is_canceled=False)
+                
+            if reserved_appointment.exists():
+                raise serializers.ValidationError('این نوبت قبلا رزرو شده است')
+                
+            try:
+                return super().create(validated_data)
+            except IntegrityError:
+                raise serializers.ValidationError('این نوبت الان رزرو شد')

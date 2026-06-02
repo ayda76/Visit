fb46fa37b5d30e8c3332bf5bb1e6bd4a64e0a117
ayda76 <aydavahedfrough@yahoo.com>
2026-06-02 17:07:25 +0100

    added permisions for schedule WorkDay

--- backend/schedule_app/api/views.py
+++ backend/schedule_app/api/views.py
@@ -4,18 +4,12 @@ from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
 
-from rest_framework import generics
-from django.db.models import Q
-from django.contrib.auth.forms import PasswordChangeForm
-from rest_framework.views import APIView
-from drf_yasg.utils import swagger_auto_schema 
-
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import action
 
 from schedule_app.api.services import add_provider
+from schedule_app.permissions import WorkDay_Permissions
 from account_app.api.serializers import *
-from account_app.models import Account
+
 
 from schedule_app.api.serializers import *
 from schedule_app.models import *
@@ -23,6 +17,7 @@ from schedule_app.models import *
 class WorkDayViewSet(viewsets.ModelViewSet):
     queryset = WorkDay.objects.all()
     serializer_class = WorkDaySerializer
+    permission_classes=[WorkDay_Permissions]
     pagination_class=None
     my_tags = ["Schedule"]
     
---
+++ backend/schedule_app/permissions.py
@@ -0,0 +1,35 @@
+from rest_framework import permissions
+
+from account_app.models import Account,Role,Status
+
+
+class WorkDay_Permissions(permissions.BasePermission):
+   
+    def has_permission(self, request,view):
+      
+        if  request.method=='POST':
+            return self.can_create_workday(request)
+        
+        return True
+        
+    def can_create_workday(self, request):
+        account_logedin= Account.get_user_jwt(self,request)
+        if account_logedin.role ==Role.DOCTOR:
+            return True
+        elif account_logedin.role ==Role.CENTER_MANAGER:
+            return True
+        elif account_logedin.role ==Role.ADMIN:
+            return True
+        else: 
+            return False
+        
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        account_logedin = Account.get_user_jwt(self, request)
+        if account_logedin.role ==Role.ADMIN:
+            return True
+        else:
+            return obj.provider_related.id == account_logedin.id     

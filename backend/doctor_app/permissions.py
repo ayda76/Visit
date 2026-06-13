1dbda01743aa803e0669bc6356c4e1efa1c878c3
ayda76 <aydavahedfrough@yahoo.com>
2026-06-13 08:42:44 +0100

    added unit test permissions doctor provider application permission and fixed permission problems

--- backend/doctor_app/permissions.py
+++ backend/doctor_app/permissions.py
@@ -40,29 +40,35 @@ class ProviderApplication_Permissions(permissions.BasePermission):
       
         if  request.method=='POST':
             return self.can_create_application(request)
-        
-        return True
+        elif request.method=='GET':
+            return self.can_get_applications(request)
+        else:
+            return True
         
     def can_create_application(self, request):
         account_logedin= Account.get_user_jwt(self,request)
         if account_logedin:
-            return True
+            return account_logedin.role in [Role.ADMIN,Role.DOCTOR_PENDING,Role.CENTER_PENDING]
         else: 
             return False
         
+    def can_get_applications(self, request):
+        account_logedin= Account.get_user_jwt(self,request)
+        if account_logedin:
+            return account_logedin.role == Role.ADMIN
+        else: 
+            return False        
+        
     def has_object_permission(self, request, view, obj):
-
-        #get is safe method
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
+          
         account_logedin = Account.get_user_jwt(self, request)
-
-        if  account_logedin.role==Role.ADMIN:
-            return True
+        if  account_logedin:
+            if  account_logedin.role==Role.ADMIN:
+                return True
+            else:
+                return account_logedin == obj.account_related
         else:
-            return account_logedin == obj.account_related
-
+            return False
 
 
 class Provider_Review_Permissions(permissions.BasePermission):

d29090c4c1c56ed254e7a63f5e5fd1f25d7a6160
ayda76 <aydavahedfrough@yahoo.com>
2026-06-25 17:49:34 +0100

    added improved version for redis cache functions in helper accounts

--- backend/account_app/api/helpers.py
+++ backend/account_app/api/helpers.py
@@ -1,5 +1,5 @@
 from django.core.cache import cache
-
+from django_redis import get_redis_connection
 
 MAX_ATTEMPTS = 5
 #15 min
@@ -16,21 +16,58 @@ def is_locked(username):
 
 
 
-def register_failed_attempt(username):
+# def register_failed_attempt(username):
 
-    key = f"login_attempts:{username}"
+#     key = f"login_attempts:{username}"
 
-    attempts = cache.get(key, 0)
+#     attempts = cache.get(key, 0)
 
-    cache.set(
-        key,
-        attempts + 1,
-        timeout=LOCK_TIME
-    )
+#     cache.set(
+#         key,
+#         attempts + 1,
+#         timeout=LOCK_TIME
+#     )
     
-    
+
+
+   
 def clear_attempts(username):
 
     cache.delete(
+        f"login_attempts:{username}"
+    )
+    
+    
+def is_locked(username):
+
+    redis = get_redis_connection("default")
+
+    key = f"login_attempts:{username}"
+
+    attempts = redis.get(key)
+
+    if attempts is None:
+        return False
+
+    return int(attempts) >= MAX_ATTEMPTS
+
+
+def register_failed_attempt(username):
+
+    redis = get_redis_connection("default")
+
+    key = f"login_attempts:{username}"
+
+    attempts = redis.incr(key)
+    if attempts == 1:
+        redis.expire(key, LOCK_TIME)
+
+    return attempts
+
+def clear_attempts(username):
+
+    redis = get_redis_connection("default")
+
+    redis.delete(
         f"login_attempts:{username}"
     )
